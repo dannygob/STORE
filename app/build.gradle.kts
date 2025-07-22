bf87b6125951339ad273f81c83c6c8d2b5264a7c
Superhola <68669782+dannygob@users.noreply.github.com>
2025-07-22 21:30:14 +0200

    fix and drop placer holder

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -109,4 +109,7 @@ dependencies {
     androidTestImplementation(libs.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+    implementation(libs.kotlin.stdlib)
+
+
 }
